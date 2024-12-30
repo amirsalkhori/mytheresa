68f2aeacf2bae7fb0b8c5ea378845ff647255152
a.salkhori <a.salkhori@SnappGrocery.com>
2024-12-30 22:07:08 +0330

    Validation for page size

--- internal/handler/product.go
+++ internal/handler/product.go
@@ -51,6 +51,10 @@ func (h *ProductHandler) GetFilteredProducts(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	if pageSize > 5 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Pagesize can not be more than 5"})
+		return
+	}
 	nextIDStr := c.Query("next")
 	var nextIDUint32 uint32
 	if nextIDStr == "" {
